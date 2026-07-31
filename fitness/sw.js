442568182feedcd3995f394876b23d4b7810bef0
12jiangjiang <1403018180@qq.com>
2026-07-31 14:42:22 +0800

    Update icons and cache: fitness/sw.js

--- fitness/sw.js
+++ fitness/sw.js
@@ -1,12 +1,12 @@
-const CACHE_NAME = 'fitness-app-v1';
+const CACHE_NAME = 'fitness-app-v2';
 const ASSETS = [
   './',
   './index.html',
   './styles.css',
   './app.js',
   './manifest.json',
-  '../icon-192.png',
-  '../icon-512.png'
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', e => {
